e9e6a8bb5ee2b1dd5ccdfcbbc9c25dbc6c2562ac
MakennaLebo <kennalebo@hotmail.com>
2024-03-06 20:44:30 -0500

    Push

--- src/components/ToDoList.js
+++ src/components/ToDoList.js
@@ -1,13 +1,21 @@
 import React, {useState} from "react";
 import TodoForm from "./TodoForm";
+import {Calendar, momentLocalizer} from 'react-big-calendar'
 
 function TodoList(){
     const [todos, setTodos] = useState ([])
+
+    const localizer = momentLocalizer(momentLocalizer)
 return (
     <div>
-        <h1>What needs to get done?</h1>
-        <TodoForm />
-    </div>
+    <Calendar
+      localizer={localizer}
+      events={myEventsList}
+      startAccessor="start"
+      endAccessor="end"
+      style={{ height: 500 }}
+    />
+  </div>
 )
 
 }
